da83fe90bbbd05d43b97b13f4eebd7d5bd966388
kawai8 <52528451+kawai8@users.noreply.github.com>
2021-10-27 18:41:44 +0900

    Update if_or.sh

--- if_or.sh
+++ if_or.sh
@@ -2,6 +2,12 @@
 
 var=$1
 
+# method 1
+# use || operator
+# Expressions may be combined using "||" operators
+# expression1 && expression2
+# True if either expression1 or expression2 is true.
+
 if [ "$var" = "a" ] \
     || [ "$var" = "b" ] \
     || [ "$var" = "c" ] \
@@ -13,5 +19,14 @@ if [ "$var" = "a" ] \
 else
     result="no match"
 fi
-
 echo $result
+
+# method 2
+# use test command option
+# expression1 -o expression2
+# either EXPRESSION1 or EXPRESSION2 is true
+
+if [ "$var1" = "foo1" -o "$var2" = "foo2" ]; then
+    echo "here"
+fi
+
