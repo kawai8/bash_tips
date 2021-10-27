0c47ea95219de2aed4b82db0d0b3f3e19dd182a3
kawai8 <52528451+kawai8@users.noreply.github.com>
2021-10-27 15:43:57 +0900

    Update if_and.sh

--- if_and.sh
+++ if_and.sh
@@ -4,6 +4,13 @@ var1="foo1"
 var2="foo2"
 var3="foo3"
 
+
+# method 1
+# use && operator
+# Expressions may be combined using "&&" operators
+# expression1 && expression2
+# True if both expression1 and expression2 are true.
+
 if [ "$var1" = "foo1" ] \
     && [ "$var2" = "foo2" ] \
     && [ "$var3" = "foo3" ]; then
@@ -11,5 +18,15 @@ if [ "$var1" = "foo1" ] \
 else
     result="no match"
 fi
-
 echo $result
+
+
+# method 2
+# use test command option
+# expression1 -a expression2
+# both EXPRESSION1 and EXPRESSION2 are true
+
+if [ "$var1" = "foo1" -a "$var2" = "foo2" ]; then
+    echo "here"
+fi
+
